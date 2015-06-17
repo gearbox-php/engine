b4f1b9894731aad4869feb06a0b31a2547fa2eb3
Gabriel Rosas <gabrielrosass@gmail.com>
2015-06-17 16:14:01 -0300

    ajuste

--- lib/engine/loader.php
+++ lib/engine/loader.php
@@ -5,10 +5,10 @@ namespace Gearbox\Engine;
 class Loader{
 
   static function classicGearLoader($class_name, $gear_name, $gear_path){
-    if(preg_match("/\AGearbox\\\$gear_name\\\([a-zA-Z\\\]*)/", "$class_name", $groups)){
+    if(preg_match("/\AGearbox\\\\".$gear_name."\\\\([a-zA-Z\\\\]*)/", "$class_name", $groups)){
       $array_class_name = explode('\\', $groups[1]);
       $array_class_name = array_map(['Gearbox\ActiveSupport','underscore'], $array_class_name);
-      $file = Gearbox\Engine::gearboxDir($gear_path.implode('/',$array_class_name).'.php');
+      $file = \Gearbox\Engine::gearboxDir($gear_path.implode('/',$array_class_name).'.php');
       if(file_exists($file)){
         require_once $file;
         return true;
